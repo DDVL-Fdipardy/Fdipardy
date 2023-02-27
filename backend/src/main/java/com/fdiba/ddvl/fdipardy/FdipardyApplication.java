c5b06948578b32ec8a97ae58d6460005bfa7b8bb
dsimeonova <d.simeonova@blubito.com>
2023-02-27 14:23:42 +0200

    backend commit

---
+++ backend/src/main/java/com/fdiba/ddvl/fdipardy/FdipardyApplication.java
@@ -0,0 +1,13 @@
+package com.fdiba.ddvl.fdipardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdipardyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdipardyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fdiba/ddvl/fdipardy/FdipardyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdiba.ddvl.fdipardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdipardyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
