8e35e7f1feb7af5ecf2de0860b1e8b14bcb1377f
Lenko Ivanov <72096565+LenkoIvanov@users.noreply.github.com>
2023-03-11 21:04:55 +0200

    feat: added a small question box

---
+++ frontend/src/Components/MainContainer/MainContainer.tsx
@@ -0,0 +1,11 @@
+import QuestionBox from "../QuestionBox/QuestionBox";
+
+const MainContainer = () => {
+  return (
+    <div>
+      <QuestionBox score={100} />
+    </div>
+  );
+};
+
+export default MainContainer;
