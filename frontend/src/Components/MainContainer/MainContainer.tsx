3543d61ddc14a0d9ff96de556c37581f84277d62
Lenko Ivanov <72096565+LenkoIvanov@users.noreply.github.com>
2023-03-13 23:01:52 +0200

    feat: added a small question table plus heading

--- frontend/src/Components/MainContainer/MainContainer.tsx
+++ frontend/src/Components/MainContainer/MainContainer.tsx
@@ -1,9 +1,29 @@
 import QuestionBox from "../QuestionBox/QuestionBox";
+import styles from "./MainContainer.module.css";
 
 const MainContainer = () => {
+  const generateQuestionBoxColumns = (): JSX.Element => {
+    const generatedBoxes: JSX.Element[] = [];
+
+    let boxScore = 100;
+    for (let i = 0; i < 4; i++) {
+      generatedBoxes.push(<QuestionBox score={boxScore} />);
+      boxScore += 100;
+    }
+
+    return <div className={styles.columnItems}>{generatedBoxes}</div>;
+  };
+
   return (
-    <div>
-      <QuestionBox score={100} />
+    <div className={styles.mainContainer}>
+      <h1 className={styles.title}>FDIPARDY</h1>
+      <div className={styles.questionsContainer}>
+        {generateQuestionBoxColumns()}
+        {generateQuestionBoxColumns()}
+        {generateQuestionBoxColumns()}
+        {generateQuestionBoxColumns()}
+        {generateQuestionBoxColumns()}
+      </div>
     </div>
   );
 };
