3100fbc30a27cb54c0467807d79ebcbd6a21933a
Denislav Kirchev <denislav_delon@abv.bg>
2023-03-27 18:00:37 +0300

    some changes added here

--- frontend/src/Components/QuestionBox/QuestionBox.tsx
+++ frontend/src/Components/QuestionBox/QuestionBox.tsx
@@ -1,10 +1,15 @@
 import { IQuestionBoxProps } from "./IQuestionBoxProps";
 import styles from "./QuestionBox.module.css";
 
+
+
 const QuestionBox = (props: IQuestionBoxProps) => {
+  
+
   return (
     <div className={styles.box} onClick={() => console.log("Question clicked")}>
       {props.score}
+      
     </div>
   );
 };
