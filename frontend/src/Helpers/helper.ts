c5252db5d08aa84cd3397b39bbc4aef811bd5bfb
LenkoIvanov17 <l.ivanov@blubito.com>
2023-03-28 21:33:23 +0300

    feat: refactor helper function

--- frontend/src/Helpers/helper.ts
+++ frontend/src/Helpers/helper.ts
@@ -3,33 +3,33 @@ import { ICategory, IFullCategory } from "../Types/ICategory";
 import { IQuestion, IQuestionWithAnswer } from "../Types/IQuestion";
 
 export const generateFullCategories = (categories: ICategory[], answers: IAnswer[]): IFullCategory[] => {
+  if (categories.length === 0 || answers.length === 0) return [];
   const questions: IQuestion[] = [];
-  const extendedQuestions: IQuestionWithAnswer[] = [];
-  const extendedCategories: IFullCategory[] = [];
 
-  categories.map((category) =>
-    category.questions.map((question) => {
+  // push all questions into one array
+  categories.forEach((category) =>
+    category.questions.forEach((question) => {
       questions.push(question);
     })
   );
 
-  questions.map((question) => {
-    answers.map((answer) => {
-      if (answer.id === question.answerId) {
-        const extendedQuestion: IQuestionWithAnswer = { ...question, answer: answer.title };
-        extendedQuestions.push(extendedQuestion);
-      }
-    });
+  // handle appending answer to relevant question
+  const extendedQuestions: IQuestionWithAnswer[] = questions.map((question) => {
+    const extendedQuestion: IQuestionWithAnswer = { ...question, answer: "" };
+    const answer: IAnswer | undefined = answers.find((answer) => answer.id === question.answerId);
+    extendedQuestion.answer = answer ? answer.title : "";
+    return extendedQuestion;
   });
 
-  categories.map((category) => {
-    const extendedCategory: IFullCategory = { id: category.id, title: category.title, questions: [] };
-    extendedQuestions.map((extendedQuestion) => {
-      if (extendedQuestion.themaId === category.id) {
-        extendedCategory.questions.push(extendedQuestion);
+  // handle creating a category with all its questions with answers
+  const extendedCategories: IFullCategory[] = categories.map((category) => {
+    const relevantFullQuestions: IQuestionWithAnswer[] = [];
+    extendedQuestions.forEach((fullQuestion) => {
+      if (fullQuestion.themaId === category.id) {
+        relevantFullQuestions.push(fullQuestion);
       }
     });
-    extendedCategories.push(extendedCategory);
+    return { id: category.id, title: category.title, questions: relevantFullQuestions };
   });
 
   return extendedCategories;
