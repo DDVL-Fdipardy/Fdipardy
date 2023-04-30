77989366395276c18d885c6bd6a5e2bb0f9aa1ad
LenkoIvanov17 <l.ivanov@blubito.com>
2023-04-30 19:16:34 +0300

    feat: Get to last working condition (no inpuy blockers)

--- frontend/src/Components/MainContainer/MainContainer.tsx
+++ frontend/src/Components/MainContainer/MainContainer.tsx
@@ -13,19 +13,9 @@ const MainContainer = () => {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [answers, setAnswers] = useState<IAnswer[]>([]);
   const [fullCategories, setFullCategories] = useState<IFullCategory[]>([]);
-  // const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-  // const [selectedQuestion, setSelectedQuestion] = useState<string>("");
-  // const [selectedAnswer, setSelectedAnswer] = useState<string>("");
-  // const [selectedQuestionScore, setSelectedQuestionScore] = useState<number>(0);
-  // const [isValid, setIsValid] = useState("");
-  // const [isSubmitting, setIsSubmitting] = useState(false);
-  // const valueRef = useRef<string>("");
   const [player1, setPlayer1] = useState<Player>({ name: "Player 1", points: 0 });
   const [player2, setPlayer2] = useState<Player>({ name: "Player 2", points: 0 });
   const [player3, setPlayer3] = useState<Player>({ name: "Player 3", points: 0 });
-  const [activePlayerIndex, setActivePlayerIndex] = useState<number>(0);
-  const players = [player1, player2, player3];
-  const [currentPlayer, setCurrentPlayer] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -63,78 +53,6 @@ const MainContainer = () => {
     );
   };
 
-  // const handleQuestionClick = (question: string, answer: string, score: number) => {
-  //   setIsModalVisible(true);
-  //   setSelectedQuestion(question);
-  //   setSelectedAnswer(answer);
-  //   setSelectedQuestionScore(score);
-  // };
-
-  // const handleClose = () => {
-  //   setIsModalVisible(false);
-  //   setSelectedQuestion("");
-  //   setSelectedAnswer("");
-  // };
-
-  // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   valueRef.current = event.target.value;
-  // };
-
-  // const updatePlayerPoints = (playerName: string, points: number) => {
-  //   switch (playerName) {
-  //     case player1.name:
-  //       setPlayer1((prevPlayer) => ({ ...prevPlayer, points: prevPlayer.points + points }));
-  //       break;
-  //     case player2.name:
-  //       setPlayer2((prevPlayer) => ({ ...prevPlayer, points: prevPlayer.points + points }));
-  //       break;
-  //     case player3.name:
-  //       setPlayer3((prevPlayer) => ({ ...prevPlayer, points: prevPlayer.points + points }));
-  //       break;
-  //   }
-  //   setCurrentPlayer(playerName);
-  // };
-
-  // const handleKeyDown = (event: KeyboardEvent) => {
-  //   const index = Number(event.key) - 1;
-  //   if (index >= 0 && index < players.length) {
-  //     setActivePlayerIndex(index);
-  //   }
-  // };
-
-  // const addKeyDownEventListener = () => {
-  //   document.addEventListener("keydown", handleKeyDown);
-  // };
-
-  // useEffect(() => {
-  //   addKeyDownEventListener();
-  // }, [players.length]);
-
-  // const handleSubmit = () => {
-  //   let pointsToAdd: number;
-  //   const inputCopy = cloneDeep(valueRef.current);
-  //   const unformattedAnswer = cloneDeep(selectedAnswer);
-  //   setIsSubmitting(true);
-  //   if (inputCopy.toLowerCase() === unformattedAnswer.toLowerCase()) {
-  //     setIsValid("True");
-  //     pointsToAdd = selectedQuestionScore;
-  //     setTimeout(() => {
-  //       setIsModalVisible(false);
-  //       setIsValid("");
-  //       setSelectedQuestion("");
-  //       setSelectedAnswer("");
-  //       setSelectedQuestionScore(0);
-  //     }, 1000);
-  //   } else {
-  //     setIsValid("False");
-  //     pointsToAdd = 0;
-  //   }
-  //   updatePlayerPoints(players[activePlayerIndex].name, pointsToAdd);
-  //   const nextPlayerIndex = (activePlayerIndex + 1) % players.length;
-  //   setActivePlayerIndex(nextPlayerIndex);
-  //   setCurrentPlayer(players[nextPlayerIndex].name);
-  // };
-
   return (
     <div className={styles.mainContainer}>
       <p className={styles.paragraph}>Welcome to</p>
@@ -147,21 +65,6 @@ const MainContainer = () => {
         <PlayerBox key={"player2"} name={player2.name} score={player2.points} color={"rgb(254, 222, 255)"} />
         <PlayerBox key={"player3"} name={player3.name} score={player3.points} color={"rgb(223, 255, 216)"} />
       </div>
-      {/* <div>
-        <QuestionModal
-          onClose={handleClose}
-          handleInputChange={handleInputChange}
-          handleSubmit={handleSubmit}
-          isSubmitting={isSubmitting}
-          players={[player1, player2, player3]}
-          activePlayerIndex={activePlayerIndex}
-          isValid={isValid}
-          valueRef={valueRef}
-          isModalVisible={isModalVisible}
-          answer={selectedAnswer}
-          question={selectedQuestion}
-        />
-      </div> */}
     </div>
   );
 };
--- frontend/src/Components/QuestionModal/QuestionModal.tsx
+++ frontend/src/Components/QuestionModal/QuestionModal.tsx
@@ -1,17 +1,47 @@
+import { cloneDeep } from "lodash";
 import { IQuestionModalProps } from "./IQuestionModalProps";
 import styles from "./QuestionModal.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const QuestionModal = (props: IQuestionModalProps) => {
   const { isModalVisible, question, isPlayerAnswerValid, onClose, handleSubmit } = props;
   const [inputValue, setInputValue] = useState<string>("");
+  const [playerIndexes, setPlatyerIndexes] = useState<number[]>([]);
+  const [activePlayerIndex, setActivePlayerIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    const playerIdxs = [1, 2, 3];
+    setPlatyerIndexes(playerIdxs);
+    addKeyDownEventListener();
+  }, []);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    const pressedKeyAsNumber = Number(event.key);
+    if (playerIndexes.includes(pressedKeyAsNumber)) {
+      setActivePlayerIndex(pressedKeyAsNumber);
+      // const idxInPlayerArray = playerIndexes.indexOf(pressedKeyAsNumber);
+      // const newIndexes = cloneDeep(playerIndexes);
+      // newIndexes.splice(idxInPlayerArray, 1);
+      // setPlatyerIndexes(newIndexes);
+      // console.log("Active index: ", pressedKeyAsNumber);
+      //console.log("Player indexesa after splice: ", newIndexes);
+    }
+  };
+
+  const addKeyDownEventListener = () => {
+    document.addEventListener("keydown", handleKeyDown);
+  };
+
+  // const removeKeyDownEventListener = () => {
+  //   document.removeEventListener("keydown", handleKeyDown);
+  // };
 
   if (!isModalVisible) {
     return <></>;
   }
 
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} id="modal">
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <div className={styles.modalHeader}>
           <h2 className={styles.modalTitle}>{question}</h2>
@@ -27,7 +57,7 @@ const QuestionModal = (props: IQuestionModalProps) => {
           {isPlayerAnswerValid === "True" && <div className="input">True answer.</div>}
         </div>
         <div className={styles.modalFooter}>
-          <h4>Active player: Implement</h4>
+          <h4>Active player: Player {activePlayerIndex}</h4>
           <button className={styles.submitButton} onClick={() => handleSubmit(inputValue)}>
             Submit
           </button>
