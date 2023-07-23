59a6d2730e159c666a6b3128c51eecf15cb21734
Lenko Ivanov <lenko.ivanov117@gmail.com>
2023-07-23 14:54:03 +0300

    feat: Make links clickable

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -3,8 +3,12 @@ import styles from "./NavBar.module.css";
 export const NavBar = () => {
   return (
     <ul className={styles.nav}>
-      <li className={styles.tab}>Game</li>
-      <li className={styles.tab}>Leaderboard</li>
+      <li className={styles.tab}>
+        <a href={"/"}>Game</a>
+      </li>
+      <li className={styles.tab}>
+        <a href={"/leaderboard"}>Leaderboard</a>
+      </li>
     </ul>
   );
 };
